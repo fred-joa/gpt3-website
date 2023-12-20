269b0dc8de6fe9c2804cbb5a68d487bfb1fd576b
fred-joa <fredjoa.bo@gmail.com>
2023-12-20 17:31:04 +0000

    Created structure project, and done Navbar and Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+ import React from 'react'
+ import './App.css'
+ 
+ import {Footer, Blog, Possibility, Features, WhatGTP3, Header} from './containers'
+ import {Brand, Cta, Navbar} from './components'
+ 
+ const App = () => {
+   return (
+     <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGTP3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+        </div>
+   )
+ }
+ export default App
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+ /*
+          ---------STOP----------   01:07 mint
+ 
+ 
+ 
+ */
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+// Easy way to group component to export all at one
+export {default as Article} from './article/Article';
+export {default as Brand} from './article/Article';
+export {default as Cta} from './cta/Cta';
+export {default as Feature} from './feature/Feature';
+export {default as Navbar} from './navbar/Navbar';
